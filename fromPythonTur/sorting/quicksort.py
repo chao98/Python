b4cad2e26a6cb1569fa488cab9ea4ceb993e2a7e
Lishan Chao <chao98@gmail.com>
2013-10-12 10:35:58 +0800

    create a directory for sorting

---
+++ fromPythonTur/sorting/quicksort.py
@@ -0,0 +1,90 @@
+#!c:\Program Files (x86)\Python33\python.exe -tt
+
+import sys
+import random
+import time
+
+def swap(L, i, j):
+	temp = L[j]
+	L[j] = L[i]
+	L[i] = temp
+
+def partition(L, low, high):
+#	mid = low + (high -  low) // 2
+#	if L[low] > L[high]:
+#		swap(L, low, high)
+#
+#	if L[mid] > L[high]:
+#		swap(L, mid, high)
+#
+#	if L[mid] > L[low]:
+#		swap(L, mid, low)
+
+	pivotkey = L[low]
+
+	while(low < high):
+		while (low < high) and (L[high] >= pivotkey):
+			high = high - 1
+
+		swap(L, low, high)
+
+		while (low < high) and (L[low] <= pivotkey):
+			low = low + 1
+
+		swap(L, low, high)
+
+	return low
+
+def qsort(L, low, high):
+	pivot = 0
+
+	if low < high:
+		pivot = partition(L, low, high)
+
+		qsort(L, low, pivot-1)
+		qsort(L, pivot+1, high)
+
+
+def main():
+	if len(sys.argv) < 3:
+		print('Usage: quicksort.py inputfile outputfile')
+		sys.exit(1)
+	
+	fileopen = open(sys.argv[1], 'r')
+
+	st = time.time()
+	print('Start time: ', st)
+	
+	line = fileopen.read()
+	L = line.split(';')
+	fileopen.close()
+	L.pop()
+
+	et1 = time.time()
+	print('End time1 (reading file, generating list): ', et1, end = ';\t')
+	print('Durtime: ', (et1 -st))
+
+	length = len(L)
+
+	qsort(L, 0, length-1)
+
+	et2 = time.time()
+	print('End time2 (quick sorting): ', et2, end = ';\t')
+	print('Durtime: ', (et2 -et1))
+
+	fileopen = open(sys.argv[2], 'w')
+
+	for i in range(length):
+		fileopen.write(L[i])
+		fileopen.write(';')
+
+	fileopen.close()
+
+	et3 = time.time()
+	print('End time3 (Writing file): ', et3, end = ';\t')
+	print('Durtime: ', (et3 -et2))
+
+	return
+
+if __name__ == '__main__':
+    main()
---
+++ fromPythonTur/sorting/randgen.py
@@ -0,0 +1,48 @@
+#!c:\Program Files (x86)\Python33\python.exe -tt
+
+import sys
+import random
+import time
+
+def main():
+	if len(sys.argv) < 3:
+		print('Usage: randgen.py num filename')
+		sys.exit(1)
+
+	loopend = int(sys.argv[1])
+	low = 999999
+	high = 10000000
+	randlist = []
+	
+	st = time.time()
+	print('Start time: ', st)
+
+	i = 0
+	while i < loopend:
+		m = random.randrange(low, high)
+		if m != 0:
+			randlist.append(m)
+		i = i + 1
+
+	et1 = time.time()
+	print('End time1 (Gen', loopend, 'random data): ', et1, end = ';\t')
+	print('Durtime: ', (et1 -st))
+
+	file = open(sys.argv[2], 'w')
+
+	i = 0
+	while i < len(randlist):
+		file.write(str(randlist[i]))
+		file.write(';')
+		i = i + 1
+
+	file.close()
+
+	et2 = time.time()
+	print('End time2 (Write', loopend, 'random data into file): ', et2, end = ';\t')
+	print('Durtime: ', (et2 -et1))
+
+	return
+
+if __name__ == '__main__':
+    main()
