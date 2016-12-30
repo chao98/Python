1e428d782ba7d81199d0e39ddf62b4a66a8d71bf
RDCCHLS <lishan.chao@ericsson.com>
2016-12-30 10:42:04 +0800

    use unittest for LastUpdateOrderedDict

---
+++ LXF/try_collections_test.py
@@ -0,0 +1,27 @@
+import unittest
+from try_collections import LastUpdateOrderedDict
+from collections import OrderedDict
+
+
+class testLastUpdateOrderedDict(unittest.TestCase):
+    def setUp(self):
+        #d = LastUpdateOrderedDict(3)
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        d = LastUpdateOrderedDict(3)
+        self.assertTrue(isinstance(d, dict))
+        self.assertTrue(isinstance(d, OrderedDict))
+        #self.assertTrue(isinstance(d, dict))
+
+    def test_setitem(self):
+        d = LastUpdateOrderedDict(3)
+        d['1'] = 1
+        self.assertEqual(d['1'], 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
