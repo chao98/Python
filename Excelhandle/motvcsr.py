0281db396f715452f2e4da8c4ebd675e4108b3b0
RDCCHLS <lishan.chao@ericsson.com>
2017-03-02 10:17:48 +0800

    enhanced csr data handling

--- Excelhandle/motvcsr.py
+++ Excelhandle/motvcsr.py
@@ -1,5 +1,18 @@
 import xlwings as xw
 from pandas import DataFrame
+from collections import namedtuple
+from datetime import datetime
+
+area = namedtuple('area', ['r1', 'c1', 'r2', 'c2'])
+
+
+def _get_time():
+    datefmt = '%Y-%m-%d'
+    timefmt = '%H:%M:%S'
+    now = datetime.now()
+    date = now.strftime(datefmt)
+    time = now.strftime(timefmt)
+    return date, time
 
 
 def _get_sheet(name):
@@ -37,19 +50,67 @@ def _get_sheet_size(sheet, row_offset=0, col_offset=0):
     # row_pace, col_pace = 512, 256
     # end_row, end_col = start_row + row_pace, start_col + col_pace
     origin_position = chr(ord('A') + start_col) + str(start_row+1)
-    print(origin_position)
+    # print(origin_position)
 
     end_col = sheet.range(origin_position).end('right').column
     end_row = sheet.range(origin_position).end('down').row
     return start_row, start_col, end_row, end_col
 
 
+def _create_df(sheet, start_row, start_col, end_row, end_col, reindex=False):
+    df = DataFrame(sheet[start_row+1:end_row, start_col:end_col].value,
+                   columns=sheet[start_row, start_col:end_col].value)
+    if reindex:
+        # df.set_index(keys=df['CSR Number - Key'], inplace=True)
+        df.set_index(keys=df.iloc[:, 0], inplace=True)
+    return df
+
+
+def _log(logsheet, msg, prefix=''):
+    date, time = _get_time()
+    col = 1
+    if logsheet:
+        log_area = area(*_get_sheet_size(logsheet))
+        row = log_area.r2 + 1
+        logsheet.range(row, col).value = date
+        logsheet.range(row, col+1).value = time
+        logsheet.range(row, col+2).value = msg
+    # print('{0} {1}, {2}, {3}'.format(prefix, date, time, msg), flush=True)
+    return
+
+
 def import_csr_data():
     from_sheet = _get_sheet('Source')
-    config_sheet = _get_sheet('Config')
-    pos = 'A16'
-    config_sheet.activate()
-    config_sheet.range(pos).value = _get_sheet_size(from_sheet, 0, 1)
+    # config_sheet = _get_sheet('Config')
+    # config_sheet.activate()
+    area1 = area(*_get_sheet_size(from_sheet, 0, 1))
+    # df1 = _create_df(from_sheet, *_get_sheet_size(from_sheet, 0, 1), reindex=True)
+    df1 = _create_df(from_sheet, *area1, reindex=True)
+    # print(df1.index.values)
+
+    raw_sheet = _get_sheet('Raw')
+    area2 = area(*_get_sheet_size(raw_sheet, 0, 0))
+    df2 = _create_df(raw_sheet, *area2, reindex=True)
+    # print(df2.index.values)
+    # print(df2.iloc[-1, 0])
+    # print(df1.index.get_loc(df2.iloc[-1, 0]))
+
+    log_sheet = _get_sheet('Log')
+    try:
+        start_index_df1 = df1.index.get_loc(df2.iloc[-1, 0]) + 1
+        end_index_df1 = df1.shape[0] + 1
+        # print(start_index_df1, end_index_df1)
+        if start_index_df1 + 1 < end_index_df1:
+            start_row = area2.r2
+            start_col = area2.c1
+            raw_sheet[start_row, start_col].value = df1.iloc[start_index_df1:end_index_df1].values
+            msg = 'Successfully copied %d lines of data!' % (end_index_df1 - start_index_df1 - 1)
+        else:
+            raise KeyError
+    except KeyError:
+        msg = 'No new data copied!'
+
+    _log(log_sheet, msg)
 
 
 def main():
